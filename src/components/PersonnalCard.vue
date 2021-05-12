65c7f9849e3feeddc09db79315b4a7853ec56d93
James Mathew <jamesmathewkkd@gmail.com>
2021-05-12 13:16:20 +0200

    initail commit

---
+++ src/components/PersonnalCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>James Mathew</h3>
+    <div class="data"><strong>E-mail:</strong> jamesmathewkkd@gmail.com</div>
+    <div class="data"><strong>Phone:</strong> +46 700730413, +91 9446087876</div>
+    <div class="data"><strong>City:</strong> Skövde, Sweden</div>
+    <div class="data"><strong>Languages:</strong> English, Malayalam, Hindi, Swedish(beginner)</div>
+    <SocialBar/>
+  </div>
+</template>
+
+<script>
+import SocialBar from './SocialBar.vue'
+
+export default {
+    name: 'PersonnalCard',
+    props: ['user', 'links'],
+    components: {
+        SocialBar,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import '@/styles/constants.scss';
+
+    .data {
+        margin-bottom: 15px;
+    }
+</style>
---
+++ src/components/SocialBar.vue
@@ -0,0 +1,54 @@
+ <template>
+  <div class="social-wrap">
+    <ul>
+      <li>
+        <a :href="facebooklink" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'facebook-square']" />
+        </a>
+      </li>
+      <li>
+        <a :href="instagram" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'instagram-square']" />
+        </a>
+      </li>
+      <li>
+        <a :href="linkedin" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
+        </a>
+      </li>
+      <li>
+        <a :href="github" target="_blank">
+          <font-awesome-icon class="icon" :icon="['fab', 'github-square']" />
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'SocialBar',
+        props: ['links'],
+        data(){
+          return{
+            facebooklink:'https://www.facebook.com/james.mathew.9889',
+            instagram : 'https://instagram.com/jameskoottumgal?igshid=1h8ps4jqmlazq',
+            linkedin : 'https://www.linkedin.com/in/james-mathew-4a8a37146/',
+            github : 'https://github.com/jamesmathewkkd'
+          }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  @import '@/styles/constants.scss';
+ .social-wrap {
+    li {
+      display: inline-block;
+      margin-right: 10px;
+    }
+    .icon {
+      font-size: 3rem;
+    }
+  }
+</style>
